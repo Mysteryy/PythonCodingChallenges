acfe2a89d1879d4df932a29faec3df39e3ae8d52
Zach <mysteryybots@gmail.com>
2024-10-25 13:41:56 -0500

    Add solution for find_index_of_first_occurrence_in_string challenge.

---
+++ challenges/easy/find_index_of_first_occurrence_in_string/solution_test.py
@@ -0,0 +1,47 @@
+import unittest
+from solution import Solution
+
+
+class SolutionTests(unittest.TestCase):
+
+    def test1(self):
+        solution = Solution()
+        input_a = "sadbutsad"
+        input_b = "sad"
+        expected_output = 0
+        self.assertEqual(solution.strStr(input_a, input_b), expected_output)
+
+    def test2(self):
+        solution = Solution()
+        input_a = "hello"
+        input_b = "ll"
+        expected_output = 2
+        self.assertEqual(solution.strStr(input_a, input_b), expected_output)
+
+    def test3(self):
+        solution = Solution()
+        input_a = "abcdef"
+        input_b = "xyz"
+        expected_output = -1  # Not found
+        self.assertEqual(solution.strStr(input_a, input_b), expected_output)
+
+    def test4(self):
+        solution = Solution()
+        input_a = "mississippi"
+        input_b = "iss"
+        expected_output = 1
+        self.assertEqual(solution.strStr(input_a, input_b), expected_output)
+
+    def test5(self):
+        solution = Solution()
+        input_a = "aabbcc"
+        input_b = "bb"
+        expected_output = 2
+        self.assertEqual(solution.strStr(input_a, input_b), expected_output)
+
+    def test6(self):
+        solution = Solution()
+        input_a = "testcase"
+        input_b = "case"
+        expected_output = 4
+        self.assertEqual(solution.strStr(input_a, input_b), expected_output)
