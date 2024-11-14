e314ae02ff832e2997bcb8350136f91936d788fc
Zach <mysteryybots@gmail.com>
2024-11-14 13:59:31 -0600

    Add solution for binary_tree_path_sum

---
+++ challenges/easy/binary_tree_path_sum/solution_test.py
@@ -0,0 +1,32 @@
+import unittest
+from solution import Solution, TreeNode
+
+
+class SolutionTests(unittest.TestCase):
+
+    def test1(self):
+        solution = Solution()
+        input_a = TreeNode(5,
+                           TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2))),
+                           TreeNode(8, TreeNode(13), TreeNode(4, None, TreeNode(1)))
+                           )
+        input_b = 22
+        expected_output = True
+        self.assertEqual(solution.hasPathSum(input_a, input_b), expected_output)
+
+    def test2(self):
+        solution = Solution()
+        input_a = TreeNode(5,
+                           TreeNode(2),
+                           TreeNode(3)
+                           )
+        input_b = 5
+        expected_output = False
+        self.assertEqual(solution.hasPathSum(input_a, input_b), expected_output)
+
+    def test3(self):
+        solution = Solution()
+        input_a = None
+        input_b = 0
+        expected_output = False
+        self.assertEqual(solution.hasPathSum(input_a, input_b), expected_output)
